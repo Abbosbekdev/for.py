6009577bad163c1d31b4fa5413f74158e368800c
Abbosbek Ustaboyev Botirjonovich <91521657+Abbosbekdev@users.noreply.github.com>
2021-10-07 10:11:22 +0500

    Create for.py
    python dasturlash tilida for loop

---
+++ for.py
@@ -0,0 +1,271 @@
+#1-masala
+
+# k,n =int(input()),int(input())
+# for x in range(n+1) :
+#     print(k)
+
+#2-masala
+
+# a,b=int(input()),int(input())
+# for x in range(a,b+1) :
+#     print(x)
+
+#3-masala
+
+# a,b=int(input()),int(input())
+# if a>b :
+#     for x in range(a-1,b,-1):
+#         print(x)
+# else:
+#     for x in range(b-1,a,-1) :
+#         print(x)
+
+#4-masala
+
+# a = float(input())
+# for x in range(1,11) :
+#     print(x*a)
+
+#5-masala
+
+# a = float(input())
+# b=0
+# for x in range(1,11):
+#     b+=0.1
+#     print(b*a)
+
+#6-masala
+
+# a = float(input())
+# b = 1
+# for x in range(5):
+#     b+=0.2
+#     print(a*b)
+
+#7-masala
+
+# a,b= int(input()),int(input())
+# yigindi= 0
+# for x in range(a,b+1) :
+#     yigindi+=x
+# print(yigindi)
+
+#8-masala
+
+# a,b= int(input()),int(input())
+# kopaytma = 1
+# for x in range(a,b+1) :
+#     kopaytma*=x
+# print(kopaytma)
+
+#9-masala
+
+# a,b= int(input()),int(input())
+# yigindi= 0
+# for x in range(a,b+1) :
+#      yigindi+=x*x
+# print(yigindi)
+
+#10-masala
+
+# a = int(input())
+# b = 1
+# yigindi = 0
+# for x in range(1,a+1) :
+#     yigindi += 1/x
+# print(yigindi)
+
+#11-masala
+# import math
+#
+# a = int(input())
+# yigindi = 0
+# for x in range(a+1) :
+#     yigindi +=math.pow(x+x,2)
+# print(yigindi)
+
+#12-masala
+
+# n = int(input())
+# kopaytma = 1
+# b=1
+# for x in range(1,n+1) :
+#    kopaytma*=b
+#    b+=0.1
+# print(kopaytma)
+
+#13-masala
+
+# n = int(input())
+# yigindi1 =0
+# yigindi2 = 0
+# b = 1.1; c = -1.2
+# for x in range(1,n+1) :
+#     yigindi1+=b
+#     b+=0.2
+#     yigindi2+=c
+#     c-=0.2
+# print(yigindi2+yigindi1)
+
+#14-masala
+
+# n = int(input())
+# yigindi = 0
+# for x in range(1,n+1) :
+#     yigindi+=(2*x-1)
+#     print(x,"ni kvadrati : ",yigindi)
+
+#15-masala
+
+# n = int(input())
+# a = int(input())
+# kopaytma =1
+# for x in range(0,n) :
+#     kopaytma*= a
+# print(a,' ni ',n, ' darajasi : ',kopaytma)
+
+#16-masasla
+
+# n = int(input())
+# a = int(input())
+# kopaytma =1
+# for x in range(1,n+1) :
+#     kopaytma*= a
+#     print(a,' ni ',x, ' darajasi : ',kopaytma)
+
+#17-masala
+
+# n = int(input())
+# a = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(0,n+1) :
+#     yigindi+= pow(a,x)
+# print(yigindi)
+
+#18-masala
+
+# n = int(input())
+# a = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(0,n+1) :
+#     yigindi+= pow(-1,x)*pow(a,x)
+# print(yigindi)
+
+#19-masala
+
+# n = int(input())
+# kopaytma =1
+# for x in range(1,n+1) :
+#     kopaytma*=x
+# print(kopaytma)
+
+#20 - masala
+
+# n = int(input())
+# a = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(1,n+1) :
+#     kopaytma*=x
+#     yigindi+=kopaytma
+# print(yigindi)
+
+#21 - masala
+
+# n = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(1,n+1) :
+#     kopaytma *=x
+#     yigindi+=1/kopaytma
+# print(yigindi)
+
+#22-masala
+
+# n = int(input())
+# a = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(1,n+1) :
+#     kopaytma*=x
+#     yigindi+=pow(a,x)/kopaytma
+# print(yigindi+1)
+
+#31-masala
+
+# n = int(input())
+# a = int(input())
+# yigindi = 0
+# y = 2
+# for x in range(1,n+1) :
+#     y = 2+1/y
+# print(y)
+
+#32-masala
+
+# n = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(1,n+1) :
+#     yigindi=(kopaytma+1)/x
+#     kopaytma = yigindi
+# print(kopaytma)
+
+#33-masala
+
+# n = int(input())
+# a1,a2= 0,1
+# for i in range(n) :
+#     c = a1+a2
+#     a1 = a2
+#     a2 = c
+#     print(c)
+
+#34-masala
+
+# n = int(input())
+# a1,a2=1,2
+# for i in range(n+1) :
+#     c = (a1+2*a2)/3
+#     a1 =a2
+#     a2 = c
+#     print(c)
+
+#35-masala
+
+# n = int(input())
+# a1,a2,a3=1,2,3
+# for x in range(n+1) :
+#     c=a2+a3-2*a1
+#     a1 = a2
+#     a2 = a3
+#     a3 = c
+#     print(c)
+
+#36-masala
+
+# n = int(input())
+# a = int(input())
+# yigindi = 0
+# kopaytma =1
+# for x in range(1,n+1) :
+#     yigindi+=pow(x,a)
+# print(yigindi)
+
+#39-masala
+
+# n = int(input())
+# for x in range(n+1) :
+#     for y in range(x) :
+#         print(x)
+
+#40-masala
+
+# n = int(input())
+# a = int(input())
+# for x in range(a,n+1) :
+#     for y in range(x) :
+#         print(x)
+
